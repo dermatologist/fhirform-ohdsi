33931b95912e57a9404a53407adf3dd2346fe7bf
Bell Eapen <github@gulfdoctor.net>
2018-08-31 14:25:07 -0400

    incomingRequestPreHandled interceptor added.

--- src/main/java/com/canehealth/interceptor/DataElementResponseInterceptor.java
+++ src/main/java/com/canehealth/interceptor/DataElementResponseInterceptor.java
@@ -1,9 +1,11 @@
 package com.canehealth.interceptor;
 
+import ca.uhn.fhir.rest.api.RestOperationTypeEnum;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.interceptor.InterceptorAdapter;
 import com.canehealth.service.InjectorService;
 import org.hl7.fhir.dstu3.model.Questionnaire;
+import org.hl7.fhir.dstu3.model.QuestionnaireResponse;
 import org.hl7.fhir.dstu3.model.Resource;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,13 @@ public class DataElementResponseInterceptor extends InterceptorAdapter {
     @Autowired
     private InjectorService injectorService;
 
+    /**
+     * Intercepts outgoing response and pass them through the injectorService
+     *
+     * @param theRequestDetails
+     * @param theResponseObject
+     * @return boolean
+     */
     @Override
     public boolean outgoingResponse(RequestDetails theRequestDetails, IBaseResource theResponseObject) {
         if (theResponseObject instanceof Resource) {
@@ -25,5 +34,24 @@ public class DataElementResponseInterceptor extends InterceptorAdapter {
         }
 
         return true;
+    }
+
+    /**
+     * Transforms any QuestionnaireResponse to CDM for storage in database
+     *
+     * @param theOperation
+     * @param theProcessedRequest
+     */
+    @Override
+    public void incomingRequestPreHandled(RestOperationTypeEnum theOperation, ActionRequestDetails theProcessedRequest) {
+        super.incomingRequestPreHandled(theOperation, theProcessedRequest);
+        IBaseResource baseResource = theProcessedRequest.getResource();
+        if (baseResource instanceof Resource) {
+            if (baseResource.getClass() == QuestionnaireResponse.class) {
+                QuestionnaireResponse questionnaireResponse = (QuestionnaireResponse) baseResource;
+                // TODO: to process the QuestionnaireResponse here.
+            }
+        }
+
     }
 }
