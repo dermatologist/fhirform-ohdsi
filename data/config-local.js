bba69a5ff1bdf01ecb359a4f11371dc75ead9d72
dermatologist <github@gulfdoctor.net>
2018-09-01 18:58:37 -0400

    config added

---
+++ data/config-local.js
@@ -0,0 +1,18 @@
+define([], function () {
+    var configLocal = {};
+
+    // clearing local storage otherwise source cache will obscure the override settings
+    localStorage.clear();
+
+    // WebAPI (TODO: Change This)
+    configLocal.api = {
+        name: 'OHDSI',
+        url: 'http://summary.nuchange.ca/WebAPI/'
+    };
+
+    configLocal.cohortComparisonResultsEnabled = false;
+    configLocal.userAuthenticationEnabled = false;
+    configLocal.plpResultsEnabled = false;
+
+    return configLocal;
+});
